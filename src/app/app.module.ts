5641aa96fe7ce74b325e235fffc05ab648315619
Ana Raballo <anitaraballo@gmail.com>
2018-03-08 12:15:32 -0500

    Inital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+//Routes Configuration
+import { RouterModule, Routes } from "@angular/router";
+import { MyHomeComponent } from './my-home/my-home.component'
+import { MyAboutComponent } from './my-about/my-about.component'
+import { ContactListComponent } from './contact-list/contact-list.component'
+import { ContactComponent} from './contact/contact.component';
+import { ContactOverviewComponent } from './contact-overview/contact-overview.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component'
+
+const routes: Routes = [
+  { path: '', component: ContactListComponent },
+  { path: 'contact/:id', component: ContactComponent},
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'about', component: MyAboutComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyHomeComponent,
+    MyAboutComponent,
+    ContactListComponent,
+    ContactComponent,
+    ContactOverviewComponent,
+    ContactEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
